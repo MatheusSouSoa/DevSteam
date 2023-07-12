d2d4cc26f3684d47c2c975993b2a6f67ee2ec420
Matheus Soares <matheus.sousoa.dev@gmail.com>
2023-07-12 20:35:36 -0300

    criado saleCard

--- src/components/cards/saleCard/caleCard.jsx
+++ src/components/cards/saleCard/caleCard.jsx
@@ -1,9 +1,23 @@
+import Button from '@/components/forms/button/button'
 import styles from './saleCard.module.css'
 
-export default function () {
+import Image from 'next/image'
+
+export default function SaleCard() {
     return (
-        <div>
-            
+        <div className={styles.salecard}>
+            <Image src='/products/league-of-legends.jpg' width={250} height={300}/>
+            <div className={styles.info}>
+                <h3 className={styles.title}>Oferta Exclusiva</h3>
+                <div className={styles.pricecard}>
+                    <div className={styles.percent}>-20%</div>
+                    <div className={styles.price}>
+                        <p className={styles.fullprice}>R$99,90</p>
+                        <h4 className={styles.discountprice}>R$89,90</h4>
+                    </div>
+                </div>
+                <Button>Adicionar ao carrinho</Button>
+            </div>
         </div>
     )
 }
